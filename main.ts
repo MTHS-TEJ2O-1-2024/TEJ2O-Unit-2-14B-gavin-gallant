c9988fc8f735c15ff457e3a3a315535f4391e417
gavin-gallant <gavin.gallant@mths.ca>
2024-11-01 11:26:30 -0400

    tryed to fix it

--- main.ts
+++ main.ts
@@ -5,4 +5,55 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+
+// when "A" is pressed, the pixels move in a squaire  
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    // move right 
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter++
+        basic.pause(500)
+    }
+        loopCounter = 0
+    // move down 
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter++
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    sprite = game.createSprite(4, 4)
+    loopCounter = 5
+    // move left 
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter--
+        basic.pause(500)
+    }
+
+    loopCounter = 0
+    // move up
+    while (loopCounter >= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter--
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
