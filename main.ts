378ced481957e811819e59dd8d75f245b76f9387
gavin-gallant <gavin.gallant@mths.ca>
2024-11-19 14:12:17 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: nov 2024
+ * This program moves led in a circle
 */
 
 // variables
@@ -10,9 +10,9 @@ let sprite: game.LedSprite = null
 let loopCounter = 0
 
 // setup
-basic.showIcon(IconNames.Happy)
 basic.pause(500)
 basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
 
 // when "A" is pressed, the pixels move in a squaire  
@@ -46,10 +46,56 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(500)
     }
 
-    loopCounter = 0
+    sprite.delete()
+    sprite = game.createSprite(0, 4)
+    loopCounter = 5
     // move up
-    while (loopCounter >= 5) {
+    while (loopCounter >= 0) {
         sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter--
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    // move down
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter++
+        basic.pause(500)
+    }
+    loopCounter = 0
+    // move right 
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter++
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    sprite = game.createSprite(4, 4)
+    loopCounter = 5
+    // move up 
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter--
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    sprite = game.createSprite(4, 0)
+    loopCounter = 5
+    // move left
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
         loopCounter--
         basic.pause(500)
     }
