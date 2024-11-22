26f1995c47326dd82491d14041493ba37b15eb49
gavin-gallant <gavin.gallant@mths.ca>
2024-11-22 11:11:50 -0500

    made the led move the other way on B

--- main.ts
+++ main.ts
@@ -59,3 +59,47 @@ input.onButtonPressed(Button.A, function () {
     sprite.delete()
     basic.showIcon(IconNames.Happy)
 })
+
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    // move down
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter++
+        basic.pause(500)
+    }
+    loopCounter = 0
+    // move right 
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter++
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    sprite = game.createSprite(4, 4)
+    loopCounter = 5
+    // move up 
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter--
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    sprite = game.createSprite(4, 0)
+    loopCounter = 5
+    // move left
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter--
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
