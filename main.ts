faaa188b242d8d821e7b959fc2818a34bcbbc772
gavin-gallant <gavin.gallant@mths.ca>
2024-11-22 11:10:18 -0500

    made it go the other way when you press B

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 
-// when "A" is pressed, the pixels move in a squaire  
+// when "A" is pressed, the pixels move in a square  
 input.onButtonPressed(Button.A, function () {
     // setup
     basic.clearScreen()
@@ -28,7 +28,7 @@ input.onButtonPressed(Button.A, function () {
         loopCounter++
         basic.pause(500)
     }
-        loopCounter = 0
+    loopCounter = 0
     // move down 
     while (loopCounter <= 5) {
         sprite.set(LedSpriteProperty.Y, loopCounter)
@@ -52,50 +52,6 @@ input.onButtonPressed(Button.A, function () {
     // move up
     while (loopCounter >= 0) {
         sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter--
-        basic.pause(500)
-    }
-
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
-})
-
-input.onButtonPressed(Button.B, function () {
-    // setup
-    basic.clearScreen()
-    loopCounter = 0
-    sprite = game.createSprite(0, 0)
-    sprite.set(LedSpriteProperty.Y, loopCounter)
-    // move down
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter++
-        basic.pause(500)
-    }
-    loopCounter = 0
-    // move right 
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        loopCounter++
-        basic.pause(500)
-    }
-
-    sprite.delete()
-    sprite = game.createSprite(4, 4)
-    loopCounter = 5
-    // move up 
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter--
-        basic.pause(500)
-    }
-
-    sprite.delete()
-    sprite = game.createSprite(4, 0)
-    loopCounter = 5
-    // move left
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
         loopCounter--
         basic.pause(500)
     }
